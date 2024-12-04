1bcba8402a1a694aa75b13543587b76080e44229
Leon Soh <mokuyobikaji@gmail.com>
2024-12-04 16:25:40 +0800

    Add PrimeNG styling

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenubarModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  items: MenuItem[] | undefined;
+    ngOnInit(): void {
+      this.items = [
+        {label: 'Create Rules'},
+        {label: 'Edit Rules'}
+      ]
+    }
+}
